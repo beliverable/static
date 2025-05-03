9a24935533e52fef3f10c1b6e2bf0890e2c2cdaf
beliverable <bogdan.secara@gmail.com>
2025-05-04 09:52:42 +1200

    Primul

---
+++ public/src/textnode.py
@@ -0,0 +1,25 @@
+from enum import Enum
+
+class TextType(Enum):
+    TEXT = "text"
+    BOLD = "bold"
+    ITALIC = "italic"
+    CODE = "code"
+    LINK = "link"
+    IMAGE = "image"
+
+class TextNode(self):
+    def __init__(self, text, text_type, url = None):
+        self.text = text
+        self.text_type = text_type
+        self.url = url
+
+    def __eq__(self, other):
+        return (
+            self.text_type == other.text_type
+            and self.text == other.text
+            and self.url == other.url
+        )
+
+    def __repr__(self):
+        return f"TextNode({repr(self.text)}, {repr(self.text_type)}, {repr(self.url)})"
